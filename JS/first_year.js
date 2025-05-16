f8d0bc9db9f5e44ff453fd531b84de064e03b6e8
Dinakar Shanmugasundaram <dinakarsri111@gmail.com>
2025-05-16 17:13:38 +0530

    Add files via upload

---
+++ JS/first_year.js
@@ -0,0 +1,490 @@
+function first_CSE_score() {
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let BEEE = document.querySelector(".BEEE-input").value;
+  let EG = document.querySelector(".EG-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      BEEE * 3 +
+      EG * 2 +
+      CP * 3) /
+    21;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function first_CSE_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let BEEE = document.querySelector(".BEEE-input").value;
+  let EG = document.querySelector(".EG-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      BEEE * 3 +
+      EG * 2 +
+      CP * 3) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function first_ECE_score() {
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let IEEE = document.querySelector(".IEEE-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      IEEE * 2 +
+      ES * 2 +
+      CP * 3) /
+    20;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function first_ECE_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let IEEE = document.querySelector(".IEEE-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      IEEE * 3 +
+      ES * 2 +
+      CP * 3) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function first_CE_score() {
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let EG = document.querySelector(".EG-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      ES * 2 +
+      EG * 2 +
+      CP * 3) /
+    20;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function first_CE_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let tam = document.querySelector(".Tamil-input").value;
+  let eng = document.querySelector(".English-input").value;
+  let mat = document.querySelector(".Maths-input").value;
+  let phy = document.querySelector(".Physics-input").value;
+  let chem = document.querySelector(".Chemistry-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let EG = document.querySelector(".EG-input").value;
+  let CP = document.querySelector(".C-P-input").value;
+  let result =
+    (tam * 1 +
+      eng * 2 +
+      mat * 4 +
+      phy * 3 +
+      chem * 3 +
+      ES * 2 +
+      EG * 2 +
+      CP * 3) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem2_score() {
+  let tam = document.querySelector(".Tamil-input").value;
+  let FAPDE = document.querySelector(".FAPDE-input").value;
+  let NM = document.querySelector(".NM-input").value;
+  let MS = document.querySelector(".MS-input").value;
+  let PE = document.querySelector(".PE-input").value;
+  let Python = document.querySelector(".Python-input").value;
+  let DE = document.querySelector(".DE-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let result =
+    (tam * 1 +
+      FAPDE * 4 +
+      NM * 4 +
+      MS * 2 +
+      PE * 2 +
+      Python * 3 +
+      DE * 3 +
+      ES * 2) /
+    21;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem2_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let tam = document.querySelector(".Tamil-input").value;
+  let FAPDE = document.querySelector(".FAPDE-input").value;
+  let NM = document.querySelector(".NM-input").value;
+  let MS = document.querySelector(".MS-input").value;
+  let PE = document.querySelector(".PE-input").value;
+  let Python = document.querySelector(".Python-input").value;
+  let DE = document.querySelector(".DE-input").value;
+  let ES = document.querySelector(".ES-input").value;
+  let result =
+    (tam * 1 +
+      FAPDE * 4 +
+      NM * 4 +
+      MS * 2 +
+      PE * 2 +
+      Python * 3 +
+      DE * 3 +
+      ES * 2) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem3_score() {
+  let DM = document.querySelector(".DM-input").value;
+  let COA = document.querySelector(".COA-input").value;
+  let DMST = document.querySelector(".DMST-input").value;
+  let DV = document.querySelector(".DV-input").value;
+  let DS = document.querySelector(".DS-input").value;
+  let Java = document.querySelector(".Java-input").value;
+  let DMSL = document.querySelector(".DMSL-input").value;
+  let Design_Studio = document.querySelector(".Design-Studio-input").value;
+  let result =
+    (DM * 4 +
+      COA * 3 +
+      DMST * 3 +
+      DV * 4 +
+      DS * 3 +
+      Java * 3 +
+      DMSL * 2 +
+      Design_Studio * 1) /
+    23;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem3_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let DM = document.querySelector(".DM-input").value;
+  let COA = document.querySelector(".COA-input").value;
+  let DMST = document.querySelector(".DMST-input").value;
+  let DV = document.querySelector(".DV-input").value;
+  let DS = document.querySelector(".DS-input").value;
+  let Java = document.querySelector(".Java-input").value;
+  let DMSL = document.querySelector(".DMSL-input").value;
+  let Design_Studio = document.querySelector(".Design-Studio-input").value;
+  let result =
+    (DM * 4 +
+      COA * 3 +
+      DMST * 3 +
+      DV * 4 +
+      DS * 3 +
+      Java * 3 +
+      DMSL * 2 +
+      Design_Studio * 1) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem4_score() {
+  let PQT = document.querySelector(".PQT-input").value;
+  let DAA = document.querySelector(".DAA-input").value;
+  let TOC = document.querySelector(".TOC-input").value;
+  let OST = document.querySelector(".OST-input").value;
+  let AIFML = document.querySelector(".AIFML-input").value;
+  let OE1 = document.querySelector(".OE1-input").value;
+  let SS1 = document.querySelector(".SS-I-input").value;
+  let OSL = document.querySelector(".OSL-input").value;
+  let Design_Studio_II = document.querySelector(
+    ".Design-Studio-II-input"
+  ).value;
+  let AIFML_Lab = document.querySelector(".AIFML-Lab-input").value;
+  let result =
+    (PQT * 3 +
+      DAA * 3 +
+      TOC * 3 +
+      OST * 3 +
+      AIFML * 3 +
+      OE1 * 3 +
+      SS1 * 1 +
+      OSL * 1 +
+      Design_Studio_II * 1 +
+      AIFML_Lab * 2) /
+    23;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem4_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let PQT = document.querySelector(".PQT-input").value;
+  let DAA = document.querySelector(".DAA-input").value;
+  let TOC = document.querySelector(".TOC-input").value;
+  let OST = document.querySelector(".OST-input").value;
+  let AIFML = document.querySelector(".AIFML-input").value;
+  let OE1 = document.querySelector(".OE1-input").value;
+  let SS1 = document.querySelector(".SS-I-input").value;
+  let OSL = document.querySelector(".OSL-input").value;
+  let Design_Studio_II = document.querySelector(
+    ".Design-Studio-II-input"
+  ).value;
+  let AIFML_Lab = document.querySelector(".AIFML-Lab-input").value;
+  let result =
+    (PQT * 3 +
+      DAA * 3 +
+      TOC * 3 +
+      OST * 3 +
+      AIFML * 3 +
+      OE1 * 3 +
+      SS1 * 1 +
+      OSL * 1 +
+      Design_Studio_II * 1 +
+      AIFML_Lab * 2) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem5_score() {
+  let LANT = document.querySelector(".LANT-input").value;
+  let WT = document.querySelector(".WT-input").value;
+  let CD = document.querySelector(".CD-input").value;
+  let PE1 = document.querySelector(".PE1-input").value;
+  let PE2 = document.querySelector(".PE2-input").value;
+  let OE2 = document.querySelector(".OE2-input").value;
+  let CN = document.querySelector(".CN-input").value;
+  let MAD = document.querySelector(".MAD-input").value;
+  let SS2 = document.querySelector(".SS-II-input").value;
+  let Proto_Studio_I = document.querySelector(".Proto-Studio-I-input").value;
+  let WT_Lab = document.querySelector(".WT-Lab-input").value;
+  let result =
+    (LANT * 3 +
+      WT * 3 +
+      CD * 3 +
+      PE1 * 3 +
+      PE2 * 3 +
+      OE2 * 3 +
+      CN * 3 +
+      MAD * 3 +
+      SS2 * 1 +
+      Proto_Studio_I * 1 +
+      WT_Lab * 2) /
+    28;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem5_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let LANT = document.querySelector(".LANT-input").value;
+  let WT = document.querySelector(".WT-input").value;
+  let CD = document.querySelector(".CD-input").value;
+  let PE1 = document.querySelector(".PE1-input").value;
+  let PE2 = document.querySelector(".PE2-input").value;
+  let OE2 = document.querySelector(".OE2-input").value;
+  let CN = document.querySelector(".CN-input").value;
+  let MAD = document.querySelector(".MAD-input").value;
+  let SS2 = document.querySelector(".SS-II-input").value;
+  let Proto_Studio_I = document.querySelector(".Proto-Studio-I-input").value;
+  let WT_Lab = document.querySelector(".WT-Lab-input").value;
+  let result =
+    (LANT * 3 +
+      WT * 3 +
+      CD * 3 +
+      PE1 * 3 +
+      PE2 * 3 +
+      OE2 * 3 +
+      CN * 3 +
+      MAD * 3 +
+      SS2 * 1 +
+      Proto_Studio_I * 1 +
+      WT_Lab * 2) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem6_score() {
+  let CT = document.querySelector(".CT-input").value;
+  let OOSET = document.querySelector(".OOSET-input").value;
+  let PE3 = document.querySelector(".PE3-input").value;
+  let PE4 = document.querySelector(".PE4-input").value;
+  let OE3 = document.querySelector(".OE3-input").value;
+  let IOT = document.querySelector(".IOT-input").value;
+  let SS3 = document.querySelector(".SS-III-input").value;
+  let Proto_Studio_II = document.querySelector(".Proto-Studio-II-input").value;
+  let OOSEL = document.querySelector(".OOSEL-Lab-input").value;
+  let result =
+    (CT * 3 +
+      OOSET * 3 +
+      PE3 * 3 +
+      PE4 * 3 +
+      OE3 * 3 +
+      IOT * 3 +
+      SS3 * 1 +
+      Proto_Studio_II * 1 +
+      OOSEL * 2) /
+    22;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem6_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let CT = document.querySelector(".CT-input").value;
+  let OOSET = document.querySelector(".OOSET-input").value;
+  let PE3 = document.querySelector(".PE3-input").value;
+  let PE4 = document.querySelector(".PE4-input").value;
+  let OE3 = document.querySelector(".OE3-input").value;
+  let IOT = document.querySelector(".IOT-input").value;
+  let SS3 = document.querySelector(".SS-III-input").value;
+  let Proto_Studio_II = document.querySelector(".Proto-Studio-II-input").value;
+  let OOSEL = document.querySelector(".OOSEL-Lab-input").value;
+  let result =
+    (CT * 3 +
+      OOSET * 3 +
+      PE3 * 3 +
+      PE4 * 3 +
+      OE3 * 3 +
+      IOT * 3 +
+      SS3 * 1 +
+      Proto_Studio_II * 1 +
+      OOSEL * 2) /
+    ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem7_score() {
+  let HVE = document.querySelector(".HVE-input").value;
+  let PE5 = document.querySelector(".PE5-input").value;
+  let PE6 = document.querySelector(".PE6-input").value;
+  let OE4 = document.querySelector(".OE4-input").value;
+  let ISP = document.querySelector(".ISP-input").value;
+  let PWP = document.querySelector(".PWP-I-input").value;
+  let result = (HVE * 3 + PE5 * 3 + PE6 * 3 + OE4 * 3 + ISP * 3 + PWP * 2) / 17;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem7_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let HVE = document.querySelector(".HVE-input").value;
+  let PE5 = document.querySelector(".PE5-input").value;
+  let PE6 = document.querySelector(".PE6-input").value;
+  let OE4 = document.querySelector(".OE4-input").value;
+  let ISP = document.querySelector(".ISP-input").value;
+  let PWP = document.querySelector(".PWP-I-input").value;
+  let result =
+    (HVE * 3 + PE5 * 3 + PE6 * 3 + OE4 * 3 + ISP * 3 + PWP * 2) / ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem8_score() {
+  let PW2 = document.querySelector(".PW2-input").value;
+  let IT = document.querySelector(".IT-input").value;
+  let result = (PW2 * 8 + IT * 2) / 10;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
+function CSE_sem8_arrear() {
+  let ar_credit = document.querySelector(".arrear-credit-input").value;
+  let PW2 = document.querySelector(".PW2-input").value;
+  let IT = document.querySelector(".IT-input").value;
+  let result = (PW2 * 8 + IT * 2) / ar_credit;
+  let roundedresult = parseFloat(result.toFixed(3));
+  console.log(result);
+  document.querySelector(
+    ".arr-result-p"
+  ).innerHTML = `Your SGPA = ${roundedresult}`;
+}
